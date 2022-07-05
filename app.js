108d71adec898a870d96489e95c826d36c9346eb
kayyenl <yenyenlee9@gmail.com>
2022-07-06 02:11:18 +0800

    finished 3/7

--- app.js
+++ app.js
@@ -46,3 +46,9 @@
 //         console.log(i + " -> " + i)
 //     }
 // }
+
+// printing out every char in a word
+// const str = "Frontend Simplified"
+// for (let i = 0; i < str.length; i++) {
+//     console.log(str[i])
+// }
