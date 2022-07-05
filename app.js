d65dc908383282e8128e3ae000dc0b70b6f1eb79
kayyenl <yenyenlee9@gmail.com>
2022-07-06 01:45:38 +0800

    finished 2/7

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// cleaner soln
+// cleaner soln to cashier problem
 
 // let cash = 30
 // let price = 30
@@ -13,3 +13,26 @@
 // if (cash < price) {
 //     console.log("not enough money - you still owe me " + -diff + " dollars!")
 // }
+
+
+
+// ternary gymnastics
+
+// let subscribed = true
+// let loggedIn = true
+
+// let str = subscribed && loggedIn ? "ok watch the video" : "no dont watch shit"
+// console.log(str)
+
+
+
+// so cashier part 2
+
+// let cash = 40
+// let price = 30
+// let isStoreOpen = true
+// let enough = cash >= price
+
+// let resultStr = enough && isStoreOpen ? "print the receipt" : "do not give receipt"
+
+// console.log(resultStr)
