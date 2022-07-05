7c1f7294d6e45c489f7ce95ad85b00e1b8e01661
kayyenl <yenyenlee9@gmail.com>
2022-07-06 01:19:50 +0800

    my first problem done clean

---
+++ app.js
@@ -0,0 +1,15 @@
+// cleaner soln
+
+// let cash = 30
+// let price = 30
+
+// let diff = cash - price
+// if (cash > price) {
+//     console.log("You paid extra - here's " + diff + " dollars change.")
+// }
+// else if (cash === price) {
+//     console.log("You paid the exact amount, have a nice day.")
+// }
+// if (cash < price) {
+//     console.log("not enough money - you still owe me " + -diff + " dollars!")
+// }
