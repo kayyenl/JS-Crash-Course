21ebafc21d01125d308a4e637381f22ec17c9c1a
kayyenl <yenyenlee9@gmail.com>
2022-07-06 02:07:26 +0800

    cleaner format + fizzbuzz

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 // cleaner soln to cashier problem
-
 // let cash = 30
 // let price = 30
-
 // let diff = cash - price
 // if (cash > price) {
 //     console.log("You paid extra - here's " + diff + " dollars change.")
@@ -17,22 +15,34 @@
 
 
 // ternary gymnastics
-
 // let subscribed = true
 // let loggedIn = true
-
 // let str = subscribed && loggedIn ? "ok watch the video" : "no dont watch shit"
 // console.log(str)
 
 
 
 // so cashier part 2
-
 // let cash = 40
 // let price = 30
 // let isStoreOpen = true
 // let enough = cash >= price
-
 // let resultStr = enough && isStoreOpen ? "print the receipt" : "do not give receipt"
-
 // console.log(resultStr)
+
+
+// FizzBuzz problem FES 
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log(i + " -> Frontend Simplified")
+//     }
+//     else if (i % 3 === 0) {
+//         console.log(i + " -> Frontend")
+//     }
+//     else if (i % 5 === 0) {
+//         console.log(i + " -> Simplified")
+//     }
+//     else {
+//         console.log(i + " -> " + i)
+//     }
+// }
